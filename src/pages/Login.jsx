7918ea3633ab08dde89c88cf59a4308d8034ad41
Bojan SKI <codeucenje@gmail.com>
2024-10-29 10:24:35 +0100

    login page initial setup done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,52 @@
+import { useNavigate } from "react-router-dom";
+// toastify
+import { toast } from 'react-toastify'
 
 
 const Login = () => {
+  const navigate = useNavigate()
+
+  const handleLoginSubmit = e => {
+    e.preventDefault()
+
+    const enteredEmail = e.target.elements[0].value.trim()
+    const enteredPassword = e.target.elements[1].value
+
+    console.log(enteredEmail);
+    console.log(enteredPassword);
+
+  }
+
+
   return (
-    <div>Login</div>
+    <div className="login-page">
+      <div className="container">
+
+        <section className="login-form bg-white w-75 mx-auto p-5 rounded-5">
+          <form onSubmit={handleLoginSubmit}>
+
+            <div className="mb-3">
+              <label htmlFor="loginEmail" className="col-form-label fw-bolder mb-1">
+                Email adresa (elektronska pošta)
+              </label>
+              <input type="email" className="form-control" name="loginEmail" id="loginEmail" placeholder="Vaša email adresa" required />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="loginPassword" className="col-form-label fw-bolder mb-1">
+                Lozinka
+              </label>
+              <input type="password" className="form-control" name="loginPassword" id="loginPassword" placeholder="Vaša lozinka" required />
+            </div>
+
+            <button type="submit" className="btn bg-orange-hover fw-bolder text-white py-3 w-100 rounded-4">
+              Prijavite se
+            </button>
+          </form>
+        </section>
+
+      </div>
+    </div>
   )
 }
 
