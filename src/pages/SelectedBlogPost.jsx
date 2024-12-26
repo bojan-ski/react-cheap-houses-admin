6993e5a17c27748bd782736815a5be8d8a6750cb
Bojan SKI <codeucenje@gmail.com>
2024-12-26 15:09:39 +0100

    SelectedBlogPost page baners updated

--- src/pages/SelectedBlogPost.jsx
+++ src/pages/SelectedBlogPost.jsx
@@ -25,19 +25,19 @@ const SelectedBlogPost = () => {
       <div className="row">
 
         {/* row item 1 */}
-        <div className="col-2 text-center">
+        <div className={`col-6 col-lg-2 text-center order-1 ${selectedBlogPost.newBlogPostPromoImgOneUrl && 'mb-5 mb-lg-0'}`}>
           {selectedBlogPost.newBlogPostPromoImgOneUrl && (
             <img src={selectedBlogPost.newBlogPostPromoImgOneUrl} alt="promo-img" className="img-fluid rounded-3" style={{ objectFit: 'cover', height: '580px' }} />
           )}
         </div>
 
         {/* row item 2 */}
-        <div className="col-8">
+        <div className="col-12 col-lg-8 order-3 order-lg-2">
           <SelectedBlogPostContent selectedBlogPost={selectedBlogPost} />
         </div>
 
         {/* row item 3 */}
-        <div className="col-2 text-center">
+        <div className={`col-6 col-lg-2 text-center order-2 order-lg-3 ${selectedBlogPost.newBlogPostPromoImgTwoUrl && 'mb-5 mb-lg-0'}`}>
           {selectedBlogPost.newBlogPostPromoImgTwoUrl && (
             <img src={selectedBlogPost.newBlogPostPromoImgTwoUrl} alt="promo-img" className="img-fluid rounded-3" style={{ objectFit: 'cover', height: '580px' }} />
           )}
